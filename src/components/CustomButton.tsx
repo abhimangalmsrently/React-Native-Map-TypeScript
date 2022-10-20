654c7ebbb576d84bfc7ed35ccca63c213a302f7c
abhimangalmsrently <108400983+abhimangalmsrently@users.noreply.github.com>
2022-10-20 16:33:59 +0530

    prettier

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,13 +4,13 @@ import AppStyles from '../utils/AppStyle';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Colors from '../utils/Colors';
 
-const CustomButton = (props: any)  => {
+const CustomButton = (props: any) => {
   return (
     <TouchableOpacity
       style={AppStyles.buttonTheme}
       onPress={() => props.onClick()}>
       {/* <Text style={AppStyles.buttonTextTheme}>{props.title}</Text> */}
-      <Icon name="reload" size={20} color= {Colors.color} />
+      <Icon name="reload" size={20} color={Colors.white} />
     </TouchableOpacity>
   );
 };
