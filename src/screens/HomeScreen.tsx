1c795dc76025809a1319dd000b5e4a865db10f23
abhimangalmsrently <108400983+abhimangalmsrently@users.noreply.github.com>
2022-10-20 14:44:27 +0530

    Tab navigator theme changed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,12 +5,18 @@ import LocationListScreen from './LocationListScreen';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Colors from '../utils/Colors';
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
    
-      <Tab.Navigator>
+      <Tab.Navigator
+      screenOptions={{
+        tabBarInactiveTintColor: Colors.deselectColor,
+        tabBarActiveTintColor: Colors.selectedColor
+      }}>
         <Tab.Screen name = "Map View" component = {MapScreen} />
         <Tab.Screen name = "List View" component = {LocationListScreen} />
       </Tab.Navigator>
