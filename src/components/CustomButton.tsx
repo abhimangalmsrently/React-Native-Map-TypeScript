cfbde00a4ef1ecb4fdc55aa33650be0b597e72fd
abhimangalmsrently <108400983+abhimangalmsrently@users.noreply.github.com>
2022-10-27 10:27:05 +0530

    added bounce animation

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,14 +4,46 @@ import AppStyles from '../utils/AppStyle';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Colors from '../utils/Colors';
 
+import { Animated } from "react-native";
+import { useRef, useEffect } from 'react';
+
 const CustomButton = (props: any) => {
+
+  const animatedScale = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+      animatedScale.setValue(1);
+  }, []);
+
+  const markerHandler = () => {
+
+      animatedScale.setValue(0.8); 
+      Animated.spring(animatedScale, {
+          toValue: 1,
+          bounciness: 8,
+          speed: 18,
+          useNativeDriver: true
+      }).start();
+
+      props.onClick()
+
+  }
   return (
-    <TouchableOpacity
+    
+      <Animated.View style = {{transform : [{scale: animatedScale}]}}>
+
+<TouchableOpacity
       style={AppStyles.buttonTheme}
-      onPress={() => props.onClick()}>
+      onPress={markerHandler}>
+
       {/* <Text style={AppStyles.buttonTextTheme}>{props.title}</Text> */}
       <Icon name="refresh" size={24} color={Colors.white} />
+
     </TouchableOpacity>
+
+      </Animated.View>
+
+      
   );
 };
 
--- src/components/CustomMap.tsx
+++ src/components/CustomMap.tsx
@@ -1,8 +1,30 @@
-import React from "react";
-import MapView, { AnimatedRegion, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useEffect, useRef } from "react";
+import { Animated } from "react-native";
+import MapView, {Marker } from 'react-native-maps';
 import AppStyles from '../utils/AppStyle';
 
 const CustomMap = (props: any) => {
+
+    const animatedScale = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        animatedScale.setValue(1);
+    }, []);
+
+    const markerHandler = (coordinate: any) => {
+
+        animatedScale.setValue(0.8); 
+        Animated.spring(animatedScale, {
+            toValue: 1,
+            bounciness: 50,
+            speed: 10,
+            useNativeDriver: true
+        }).start();
+
+        props.onLongPressProps(coordinate);
+
+    }
+
     return (
         <MapView
         ref={props.mapRefProps}
@@ -13,10 +35,11 @@ const CustomMap = (props: any) => {
             initialRegion={props.initialRegionProps}
             region = {props.regionProps}
 
-            onLongPress={(coordinate) => props.onLongPressProps(coordinate)}>
+            onLongPress={(coordinate) => markerHandler(coordinate)}>
 
-            {props.locationListProps.map((marker: any) => (
-                <Marker
+            {props.locationListProps.map((marker: any, index: any) => (
+               <Animated.View style = {{transform : [{scale: animatedScale}]}}>
+                 <Marker key={index}
                     coordinate={{
                         latitude: marker.latitude,
                         longitude: marker.longitude,
@@ -24,6 +47,7 @@ const CustomMap = (props: any) => {
                     title={marker.title}
                     description={marker.Description}
                 />
+               </Animated.View>
             ))}
         </MapView>
     )
