18162b017a35f0acc3aeb20d9d96c84203c00cc2
abhimangalmsrently <108400983+abhimangalmsrently@users.noreply.github.com>
2022-11-01 14:51:56 +0530

    changed Description to description

--- src/utils/MarkerLocations.ts
+++ src/utils/MarkerLocations.ts
@@ -4,13 +4,13 @@ export const locationsList = [
     latitude: 10.073232,
     longitude: 76.302765,
     title: 'Ernakulam',
-    Description: 'This is Kerala',
+    description: 'This is Kerala',
   },
   {
     key: 1,
     latitude: 10.530345,
     longitude: 76.214729,
     title: 'Thrissur',
-    Description: 'This is Kerala',
+    description: 'This is Kerala',
   },
 ];
